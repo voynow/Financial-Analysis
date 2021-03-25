64cf169ad50a60b8eef28fcb09a4a5864be66a8b
mkoretsky1 <54589102+mkoretsky1@users.noreply.github.com>
2021-03-24 20:02:40 -0400

    Updating data pipeline for multiple files

--- data_pipeline.py
+++ data_pipeline.py
@@ -13,15 +13,36 @@ from fill_nans import fill_nans
 
 
 def run_pipeline(file):
-
-    # Read raw data from yfinance api
-    df = pd.read_csv(file)
-
-    # preprocessing on raw data
-    df = preprocess(df)
-
-    # removing nan values
-    df = fill_nans(df)
+    if type(file) is str: # One file
+        # Read raw data from yfinance api
+        df = pd.read_csv(file)
+    
+        # preprocessing on raw data
+        df = preprocess(df)
+    
+        # removing nan values
+        df = fill_nans(df)
+    else: # Multiple files
+        # Read raw data from yfinance api for first file in list
+        df = pd.read_csv(file[0])
+        
+        # preprocessing on raw data
+        df = preprocess(df)
+        
+        # removing nan values
+        df = fill_nans(df)
+        
+        # looping over the rest of the files in the list
+        for i in range(1, len(file)):
+            # Read raw data
+            next_df = pd.read_csv(file[i])
+            # preprocess
+            next_df = preprocess(next_df)
+            # fill nans
+            next_df = fill_nans(next_df)
+            # concatenate to df
+            # inner join: only shared columns are kept
+            df = pd.concat([df, next_df], join="inner")   
 
     # now data is prepared to model
     return df
